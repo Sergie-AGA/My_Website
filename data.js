07e192769ebbe58156aab5f134572a3e8d931ae1
Sergie <sergiearruda@gmail.com>
2021-03-12 22:27:46 +0000

    Creates the basic testimonial data

---
+++ data.js
@@ -0,0 +1,23 @@
+const testimonialsData = [
+  {
+    id: 0,
+    name: "Giuliana Murakami",
+    img: "",
+    job: "Lawyer and writer",
+    linkedin: "https://linkedin.com/in/giuliana-murakami-14a52bb8",
+    experience:
+      "We worked together as managers of the research groups for the student organization LAJUPA, where we coordinated the research activities of over 80 law students",
+    testimonial:
+      "I have worked with Sergie in the management of research groups in LAJUPA. I soon noticed he was a born leader, not only by the brilliant solutions he presented as well as the kind way he treated others. He is an exemplary person, someone whom I would recommend to anyone.",
+  },
+  {
+    id: 1,
+    name: "Bernardo Pita",
+    img: "",
+    job: "Performance Marketing Manager and designer",
+    linkedin: "",
+    experience:
+      "I created his website and I developed a website for a client with him as the designer in the team",
+    testimonial: "",
+  },
+];
